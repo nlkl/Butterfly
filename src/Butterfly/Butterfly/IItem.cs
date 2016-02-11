c542cc0187fbafc9467e5014cadc933a3a411b09
Nils Lück <nils.luck@outlook.com>
2016-02-11 23:13:13 +0100

    Started working on item API

--- src/Butterfly/Butterfly/IItem.cs
+++ src/Butterfly/Butterfly/IItem.cs
@@ -1,5 +1,8 @@
-﻿using Sitecore.Data;
+﻿using Optional;
+using Sitecore.Data;
 using Sitecore.Data.Items;
+using Sitecore.Globalization;
+using Sitecore.Links;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +14,35 @@ namespace Butterfly
     public interface IItem
     {
         Item InnerItem { get; }
+
         ID Id { get; }
+        ID TemplateId { get; }
+        IEnumerable<ID> TemplateIds { get; }
 
         string Name { get; }
         string DisplayName { get; }
 
-        // TODO: Add rest of interface
+        ItemUri Uri { get; }
+        string DatabaseName { get; }
+        string LanguageName { get; }
+        string Path { get; }
+        int Version { get; }
+        bool IsLatestVersion { get; }
+
+        string GenerateUrl();
+        string GenerateUrl(UrlOptions options);
+
+        Option<IItem> Parent();
+        Option<TItem> Parent<TItem>() where TItem : IItem;
+        Option<IItem> NextSibling();
+        Option<TItem> NextSibling<TItem>() where TItem : IItem;
+        Option<IItem> PreviousSibling();
+        Option<TItem> PreviousSibling<TItem>() where TItem : IItem;
+        IEnumerable<IItem> Children();
+        IEnumerable<TItem> Children<TItem>() where TItem : IItem;
+        IEnumerable<IItem> Ancestors();
+        IEnumerable<TItem> Ancestors<TItem>() where TItem : IItem;
+        IEnumerable<IItem> Descendants();
+        IEnumerable<TItem> Descendants<TItem>() where TItem : IItem;
     }
 }
--- src/Butterfly/Butterfly/TypedItem.cs
+++ src/Butterfly/Butterfly/TypedItem.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Sitecore.Data;
 using Sitecore.Data.Items;
+using Optional;
+using Butterfly.Mapping;
+using Sitecore.Globalization;
+using Sitecore.Links;
 
 namespace Butterfly
 {
@@ -12,16 +16,71 @@ namespace Butterfly
     {
         private readonly Item innerItem;
 
-        public ID Id => InnerItem.ID;
         public Item InnerItem => innerItem;
+
+        public ID Id => InnerItem.ID;
+        public ID TemplateId => InnerItem.TemplateID;
+
+        public IEnumerable<ID> TemplateIds
+        {
+            get
+            {
+                var templateIds = InnerItem
+                    .Template?
+                    .BaseTemplates?
+                    .Select(t => t.ID)?
+                    .Distinct()?
+                    .ToArray();
+
+                return templateIds ?? Enumerable.Empty<ID>();
+            }
+        }
 
         public string Name => InnerItem.Name;
         public string DisplayName => InnerItem.DisplayName;
+
+        public ItemUri Uri => InnerItem.Uri;
+        public string DatabaseName => InnerItem.Database.Name;
+        public string LanguageName => InnerItem.Language.Name;
+        public string Path => InnerItem.Paths.FullPath;
+        public int Version => InnerItem.Version.Number;
+        public bool IsLatestVersion => InnerItem.Versions.IsLatestVersion();
 
         public TypedItem(Item item)
         {
             if (item == null) throw new ArgumentNullException(nameof(item));
             this.innerItem = item;
         }
+
+        public virtual string GenerateUrl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual string GenerateUrl(UrlOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Option<IItem> Parent() => innerItem.Parent.SomeNotNull().Map(MapItem);
+        public Option<TItem> Parent<TItem>() where TItem : IItem => InnerItem.Parent.SomeNotNull().FlatMap(MapItemAs<TItem>);
+
+        public Option<IItem> NextSibling() => InnerItem.Axes.GetNextSibling().SomeNotNull().Map(MapItem);
+        public Option<TItem> NextSibling<TItem>() where TItem : IItem => InnerItem.Axes.GetNextSibling().SomeNotNull().FlatMap(MapItemAs<TItem>);
+
+        public Option<IItem> PreviousSibling() => InnerItem.Axes.GetPreviousSibling().SomeNotNull().Map(MapItem);
+        public Option<TItem> PreviousSibling<TItem>() where TItem : IItem => InnerItem.Axes.GetPreviousSibling().SomeNotNull().FlatMap(MapItemAs<TItem>);
+
+        public IEnumerable<IItem> Children() => InnerItem.Children.Select(MapItem).ToArray();
+        public IEnumerable<TItem> Children<TItem>() where TItem : IItem => Children().OfType<TItem>();
+
+        public IEnumerable<IItem> Ancestors() => InnerItem.Axes.GetAncestors().Select(MapItem).ToArray();
+        public IEnumerable<TItem> Ancestors<TItem>() where TItem : IItem => Ancestors().OfType<TItem>();
+
+        public IEnumerable<IItem> Descendants() => InnerItem.Axes.GetDescendants().Select(MapItem).ToArray();
+        public IEnumerable<TItem> Descendants<TItem>() where TItem : IItem => Descendants().OfType<TItem>();
+
+        protected virtual IItem MapItem(Item item) => TemplateMapper.Map(item);
+        protected virtual Option<TItem> MapItemAs<TItem>(Item item) where TItem : IItem => TemplateMapper.MapAs<TItem>(item);
     }
 }
