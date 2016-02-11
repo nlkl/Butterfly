7db18a7ec065b42b92a7ebd0a63c02d0e3e24abf
Nils Lück <nils.luck@outlook.com>
2016-02-11 21:34:16 +0100

    A bit af scaffolding for renderable fields

---
+++ src/Butterfly/Butterfly.Web/Fields/IRenderableField.cs
@@ -0,0 +1,14 @@
+﻿using Butterfly.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Butterfly.Web.Fields
+{
+    public interface IRenderableField : IField, IHtmlString
+    {
+    }
+}
---
+++ src/Butterfly/Butterfly.Web/Fields/RenderableField.cs
@@ -0,0 +1,19 @@
+﻿using Butterfly.Fields;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Butterfly.Web.Fields
+{
+    public class RenderableField : TypedField, IRenderableField
+    {
+        public RenderableField(Item item, string fieldName) : base(item, fieldName)
+        {
+        }
+
+        public string ToHtmlString() => Renderer.Render().Result;
+    }
+}
